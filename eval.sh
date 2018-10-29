0052b094fa459ef8ac4ff23844eb9ffbc152da6c
Garrett Wilson <g@floft.net>
2018-10-28 23:32:51 -0700

    SSD not SSD Lite
    Seems to work better

--- eval.sh
+++ eval.sh
@@ -9,4 +9,4 @@ export CUDA_VISIBLE_DEVICES= # Run evaluation on CPU since GPU used by training
 python3 object_detection/model_main.py \
         --model_dir="../../object_detection_models" \
         --checkpoint_dir="../../object_detection_models" \
-        --pipeline_config_path="../../ssdlite_mobilenet_v1_coco.config"
+        --pipeline_config_path="../../ssd_mobilenet_v1_coco.config"
--- train.sh
+++ train.sh
@@ -9,4 +9,4 @@ protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH="$PYTHONPATH:$(pwd):$(pwd)/slim"
 python3 object_detection/model_main.py \
         --model_dir="../../object_detection_models" \
-        --pipeline_config_path="../../ssdlite_mobilenet_v1_coco.config"
+        --pipeline_config_path="../../ssd_mobilenet_v1_coco.config"
