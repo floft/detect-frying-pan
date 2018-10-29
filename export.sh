c5201e85e0d2c969ee7b62d380237fb4f46ea861
Garrett Wilson <g@floft.net>
2018-10-28 23:04:50 -0700

    Export and optimize TF Lite model

--- export.sh
+++ export.sh
@@ -7,6 +7,7 @@ cd models/research
 export PYTHONPATH="$PYTHONPATH:$(pwd):$(pwd)/slim"
 export CUDA_VISIBLE_DEVICES=
 
+# Export latest checkpoint
 lastCheckpoint="$(ls ../../object_detection_models/model.ckpt-* | \
         sort | tail -n 1 | sed -r 's#\.[^\.]+$##g')"
 
@@ -15,3 +16,26 @@ python3 object_detection/export_tflite_ssd_graph.py \
     --trained_checkpoint_prefix="$lastCheckpoint" \
     --output_directory="../../exported_models" \
     --add_postprocessing_op=true
+
+# Optimize - quantized (errors since we need min/max values for quantization)
+#toco \
+#    --graph_def_file="../../exported_models/tflite_graph.pb" \
+#    --output_file="../../exported_models/detect.tflite" \
+#    --input_shapes=1,300,300,3 \
+#    --input_arrays=normalized_input_image_tensor \
+#    --output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3' \
+#    --inference_type=QUANTIZED_UINT8 \
+#    --mean_values=128 \
+#    --std_dev_values=128 \
+#    --change_concat_input_ranges=false \
+#    --allow_custom_ops
+
+# Optimize - floating point
+toco \
+    --graph_def_file="../../exported_models/tflite_graph.pb" \
+    --output_file="../../exported_models/detect.tflite" \
+    --input_shapes=1,300,300,3 \
+    --input_arrays=normalized_input_image_tensor \
+    --output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3'  \
+    --inference_type=FLOAT \
+    --allow_custom_ops
