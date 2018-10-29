e24e5f12495403d2aaaf4b5df8031f310bc00319
Garrett Wilson <g@floft.net>
2018-10-28 21:16:37 -0700

    Eval only on separate job
    Don't evaluate in train.sh, only in eval.sh

--- train.sh
+++ train.sh
@@ -8,5 +8,6 @@ protoc object_detection/protos/*.proto --python_out=.
 
 export PYTHONPATH="$PYTHONPATH:$(pwd):$(pwd)/slim"
 python3 object_detection/model_main.py \
+        --noeval_training_data \
         --model_dir="../../object_detection_models" \
         --pipeline_config_path="../../ssd_mobilenet_v1_coco.config"
